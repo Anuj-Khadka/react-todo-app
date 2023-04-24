94208c7c298b50a3888342fbfc191c1265072aea
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-24 23:40:32 +0545

    update todo app

---
+++ public/variables.js
@@ -0,0 +1,8 @@
+colors={
+    primaryBackground: "#171723",
+    secondarBackground: "#24273D",
+    primaryColor:"#FCFCF8",
+    secondaryColor: "#AFB2D8",
+    tertiaryColor: "#53556C",
+    selected: "#407CF5",
+}
