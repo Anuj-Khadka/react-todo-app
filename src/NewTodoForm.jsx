f56f24b475eaf55df1119eeb6c43e6a9cd1255d8
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-24 23:40:07 +0545

    update todo app

--- src/NewTodoForm.jsx
+++ src/NewTodoForm.jsx
@@ -1,32 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import { MdAddCircleOutline } from "react-icons/md";
 
-const NewTodoForm = () => {
+const NewTodoForm = ({ onSubmit }) => {
   const [newItem, setNewItem] = useState("");
-  const [todos, setTodos] = useState([]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    setTodos((currentTodos) => {
-      return [
-        ...currentTodos,
-        { id: crypto.randomUUID(), title: newItem, completed: false },
-      ];
-    });
+    if (newItem === "") return;
+    onSubmit(newItem);
     setNewItem("");
   };
 
   return (
     <form className="new-item-form" onSubmit={handleSubmit}>
       <div className="row">
-        <label htmlFor="item">New Item</label>
+        {/* <label htmlFor="item">New Item</label> */}
+        <span className="btn">
+          <MdAddCircleOutline size="1.8rem"/>
+        </span>
         <input
           type="text"
           id="item"
+          placeholder="Add a new item..."
+          autoComplete="off"
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
         />
       </div>
-      <button className="btn">Add</button>
     </form>
   );
 };
