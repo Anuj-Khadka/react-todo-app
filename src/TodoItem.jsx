ab991536082e8c0021d27df239de4d6365a5ca51
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-25 22:38:37 +0545

    update todo app

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -4,16 +4,25 @@ import { MdDeleteOutline } from "react-icons/md";
 const TodoItem = ({ completed, id, title, toggleTodo, deleteTodo }) => {
   return (
     <li key={id}>
-      <label htmlFor="">
+      {/* <label htmlFor="">
         <input
           type="checkbox"
           checked={completed}
           onChange={(e) => toggleTodo(id, e.target.checked)}
         />
         {title}
+      </label> */}
+      <label className="checkbox-container">
+        <input
+          type="checkbox"
+          checked={completed}
+          onChange={(e) => toggleTodo(id, e.target.checked)}
+        />
+        <p className="todo-item">{title}</p>
+        <span className="checkmark"></span>
       </label>
       <span className="btn btn-danger" onClick={() => deleteTodo(id)}>
-        <MdDeleteOutline size="1.5rem"/>
+        <MdDeleteOutline size="1.5rem" />
       </span>
     </li>
   );
