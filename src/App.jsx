5714a99aa7048b5e21f89777a99d12f32138500e
Anuj-Khadka <anujkhadka008@gmail.com>
2023-04-26 23:26:38 +0545

    update todo app

--- src/App.jsx
+++ src/App.jsx
@@ -41,10 +41,9 @@ function App() {
     });
   };
 
-
-  const clearCompleted= ()=> {
+  const clearCompleted = () => {
     setTodos((prevTodos) => prevTodos.filter((todo) => !todo.completed));
-  }
+  };
 
   return (
     <div className="container">
@@ -58,7 +57,10 @@ function App() {
           toggleTodo={toggleTodo}
           deleteTodo={deleteTodo}
         />
-        <Summary todos={todos} clearCompleted={clearCompleted} />
+        <Summary
+          todos={todos}
+          clearCompleted={clearCompleted}
+        />
       </div>
     </div>
   );
